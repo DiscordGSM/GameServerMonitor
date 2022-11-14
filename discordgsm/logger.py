f9bac81ef3c28f00b89e04b13ef6f7d35b50fa10
BattlefieldDuck <29337428+BattlefieldDuck@users.noreply.github.com>
2022-11-15 07:17:27 +0800

    Update logging

--- discordgsm/logger.py
+++ discordgsm/logger.py
@@ -1,22 +1,36 @@
-import io
+import logging
 import os
 from datetime import datetime
-from pathlib import Path
+from logging.handlers import TimedRotatingFileHandler
 
-from discord import Interaction
+from discord import Interaction, utils
+from discord.utils import _ColourFormatter, stream_supports_colour
 from dotenv import load_dotenv
 
-try:
-    from zoneinfo import ZoneInfo
-except ImportError:
-    from backports.zoneinfo import ZoneInfo
-
 load_dotenv()
+
+filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data', 'logs', '{:%Y-%m-%d}.txt'.format(datetime.now()))
+file_handler = TimedRotatingFileHandler(filename=filename, when='D', encoding='utf-8')
+utils.setup_logging(handler=file_handler, root=True)
+
+handler = logging.StreamHandler()
+
+if isinstance(handler, logging.StreamHandler) and stream_supports_colour(handler.stream):
+    formatter = _ColourFormatter()
+else:
+    dt_fmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+
+handler.setFormatter(formatter)
+
+library, _, _ = __name__.partition('.')
+logger = logging.getLogger(library)
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 
 class Logger:
     """Custom Logger"""
-    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data', 'logs')
 
     @staticmethod
     def command(interaction: Interaction, *args, **kwargs):
@@ -27,42 +41,17 @@ class Logger:
 
     @staticmethod
     def info(*args, **kwargs):
-        Logger.__print_and_write_with_tz('INFO', '94', *args, **kwargs)
+        logger.info(*args, **kwargs)
 
     @staticmethod
     def warning(*args, **kwargs):
-        Logger.__print_and_write_with_tz('WARNING', '93', *args, **kwargs)
+        logger.warning(*args, **kwargs)
 
     @staticmethod
     def error(*args, **kwargs):
-        Logger.__print_and_write_with_tz('ERROR', '91', *args, **kwargs)
+        logger.error(*args, **kwargs)
 
     @staticmethod
     def debug(*args, **kwargs):
         if os.getenv('APP_DEBUG', '').lower() == 'true':
-            Logger.__print_and_write_with_tz('DEBUG', '36', *args, **kwargs)
-
-    @staticmethod
-    def __get_timestamp_with_zone_info():
-        return datetime.now(ZoneInfo(os.getenv('TZ'))) if os.getenv('TZ') and os.getenv('TZ').strip() else datetime.now()
-
-    @staticmethod
-    def __print_and_write_with_tz(info: str, color: str, *args, **kwargs):
-        timestamp = Logger.__get_timestamp_with_zone_info()
-
-        if os.getenv('HEROKU_APP_NAME') is not None:
-            print(info.ljust(8), *args, **kwargs)
-        else:
-            print(f'\x1b[1m\x1b[90m{timestamp.strftime("%Y-%m-%d %H:%M:%S")}\x1b[0m', f'\x1b[1m\x1b[{color}m{info.ljust(8)}\x1b[0m', *args, **kwargs)
-
-        Path(Logger.path).mkdir(parents=True, exist_ok=True)
-
-        with open(os.path.join(Logger.path, f'{timestamp.date()}.txt'), 'a', encoding='utf-8') as f:
-            f.write(Logger.__print_to_string(timestamp.strftime("%Y-%m-%d %H:%M:%S"), info.ljust(8), *args, **kwargs))
-
-    # Credits: https://stackoverflow.com/questions/39823303/python3-print-to-string
-    @staticmethod
-    def __print_to_string(*args, **kwargs):
-        with io.StringIO() as output:
-            print(*args, file=output, **kwargs)
-            return output.getvalue()
+            logger.debug(*args, **kwargs)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+
 from discordgsm import client
 
 if __name__ == '__main__':

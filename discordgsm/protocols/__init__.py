20e91da560190f662660d16a4fff2a58df364ff4
Battlefield Duck <hkdsethomas@gmail.com>
2023-02-22 01:50:56 +0800

    Update protocols

--- discordgsm/protocols/__init__.py
+++ discordgsm/protocols/__init__.py
@@ -29,40 +29,34 @@ from .won import WON
 class Protocols:
     @staticmethod
     def get(protocol_name: str, kv: dict):
-        address = str(kv['host'])
-        query_port = int(str(kv['port']))
+        address, query_port = str(kv['host']), int(str(kv['port']))
 
-        if kv['type'] == 'terraria':
-            return Terraria(address, query_port, str(kv['_token']))
-        elif kv['type'] == 'minecraft':
-            return Minecraft(address, query_port)
-        elif kv['type'] == 'eco':
-            return Eco(address, query_port)
-        elif kv['type'] == 'minecraftpe':
-            return Raknet(address, query_port)
-        elif kv['type'] == 'discord':
-            return Discord(address)
-        elif kv['type'] == 'hexen2':
-            return Hexen2(address, query_port)
-        elif kv['type'] == 'gportal':
-            return GPortal(address, query_port, str(kv['serverId']))
-
+        # match case statements only support on python 3.10
+        # so if elif is used for python 3.8+ compatible
         if protocol_name == 'ase':
             return ASE(address, query_port)
         elif protocol_name == 'assettocorsa':
             return AssettoCorsa(address, query_port)
         elif protocol_name == 'battlefield':
             return Battlefield(address, query_port)
-        elif protocol_name == 'valve':
-            return Source(address, query_port)
+        elif protocol_name == 'discord':
+            return Discord(address)
         elif protocol_name == 'doom3':
             return Doom3(address, query_port)
+        elif protocol_name == 'eco':
+            return Eco(address, query_port)
         elif protocol_name == 'gamespy1':
             return GameSpy1(address, query_port)
         elif protocol_name == 'gamespy2':
             return GameSpy2(address, query_port)
         elif protocol_name == 'gamespy3':
             return GameSpy3(address, query_port)
+        elif protocol_name == 'gportal':
+            return GPortal(address, query_port, str(kv['serverId']))
+        elif protocol_name == 'hexen2':
+            return Hexen2(address, query_port)
+        elif protocol_name == 'minecraft':
+            return Minecraft(address, query_port)
         elif protocol_name == 'fivem':
             return FiveM(address, query_port)
         elif protocol_name == 'quake1':
@@ -71,19 +65,25 @@ class Protocols:
             return Quake2(address, query_port)
         elif protocol_name == 'quake3':
             return Quake3(address, query_port)
+        elif protocol_name == 'raknet':
+            return Raknet(address, query_port)
         elif protocol_name == 'samp':
             return Samp(address, query_port)
         elif protocol_name == 'scum':
             return Scum(address, query_port)
+        elif protocol_name == 'source':
+            return Source(address, query_port)
         elif protocol_name == 'unreal2':
             return Unreal2(address, query_port)
         elif protocol_name == 'teamspeak3':
             return Teamspeak3(address, int(str(kv['teamspeakQueryPort'])), query_port)
+        elif protocol_name == 'terraria':
+            return Terraria(address, query_port, str(kv['_token']))
         elif protocol_name == 'ut3':
             return UT3(address, query_port)
         elif protocol_name == 'vcmp':
             return Vcmp(address, query_port)
-        elif protocol_name == 'goldsrc':
+        elif protocol_name == 'won':
             return WON(address, query_port)
 
         return None
